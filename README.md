14a4c76374a9602d62db37ce0b54fe9cbdf1b94d
ckron <7393106+ckron@users.noreply.github.com>
2022-12-28 19:12:03 +0900

    :dvd: lets glut

--- README.md
+++ README.md
@@ -7,3 +7,8 @@
 4. cd /root/workspace/src
 5. g++ -Wall -Weffc++ main.cpp -lglut -lGLU -lGL -o res.out
 6. ./res.out
+
+### reference
+1. https://www.webcyou.com/?p=9639
+2. http://www.oit.ac.jp/is/L231/pukiwiki/index.php?%E6%84%9F%E8%A6%9A%E3%83%A1%E3%83%87%E3%82%A3%E3%82%A2%E7%A0%94%E7%A9%B6%E5%AE%A4/OpenGL/%E3%83%86%E3%82%AF%E3%82%B9%E3%83%81%E3%83%A3%E3%83%9E%E3%83%83%E3%83%94%E3%83%B3%E3%82%B0
+3. https://marina.sys.wakayama-u.ac.jp/~tokoi/?date=20050614
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,123 @@
 #include <GL/glut.h>
 
-void displayMe(void)
+#include <stdio.h>
+#include <fstream>
+
+#define TEX_HEIGHT 960 
+#define TEX_WIDTH  679
+#define TEX_CHANNEL 3
+
+#define TEXTURE_FILENAME "src.txt"
+
+static GLubyte img[TEX_HEIGHT][TEX_WIDTH][TEX_CHANNEL];
+int tex_buf[TEX_HEIGHT * TEX_WIDTH * TEX_CHANNEL];
+
+void readTextureData()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POLYGON);
-        glVertex3f(0.5, 0.0, 0.5);
-        glVertex3f(0.5, 0.0, 0.0);
-        glVertex3f(0.0, 0.5, 0.0);
-        glVertex3f(0.0, 0.0, 0.5);
-    glEnd();
-    glFlush();
+  FILE *fp;
+  char buf[1024];
+  fp = fopen(TEXTURE_FILENAME, "r");
+
+  int counter = 0;
+  while(fgets(buf, 1023, fp) != NULL) {
+    if(counter == 0 || counter == 1 || counter == 2 || counter == 3) {
+      counter++;
+      continue;
+    }
+
+    tex_buf[counter - 4] = atoi(buf);
+
+    counter++;
+  }
+
+  return;
 }
 
-int main(int argc, char** argv)
+void setTextureData()
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE);
-    glutInitWindowSize(400, 300);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Hello world!");
-    glutDisplayFunc(displayMe);
-    glutMainLoop();
-    return 0;
+  // 事前に読み込んだテクスチャデータを読み込む
+  // ※ ここの処理で画像データからRGBを配列に入れている
+  // 画像データがさし変わる場合にはこの処理を繰り返す
+  for(int y=0; y<TEX_HEIGHT; y++) {
+    for(int x=0; x<TEX_WIDTH; x++) {
+      img[y][x][0] = tex_buf[y * TEX_WIDTH * TEX_CHANNEL + x * TEX_CHANNEL + 0];
+      img[y][x][1] = tex_buf[y * TEX_WIDTH * TEX_CHANNEL + x * TEX_CHANNEL + 1];
+      img[y][x][2] = tex_buf[y * TEX_WIDTH * TEX_CHANNEL + x * TEX_CHANNEL + 2];
+    }
+  }
+
+  return;
+}
+
+void init()
+{
+  readTextureData();
+  setTextureData();
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glDepthFunc(GL_LEQUAL);
+  glEnable(GL_DEPTH_TEST);
+  glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, TEX_WIDTH, TEX_HEIGHT,   
+            0, GL_RGB, GL_UNSIGNED_BYTE, img);
+
+  return;
+}
+
+void display(void)
+{
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glEnable(GL_TEXTURE_2D);
+  glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 
+          TEX_WIDTH, TEX_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, img);
+  glBegin(GL_POLYGON);
+    glTexCoord2d(0.0, 1.0); glVertex2f(-1.0, -1.0);
+    glTexCoord2d(0.0, 0.0); glVertex2f(-1.0, 1.0);
+    glTexCoord2d(1.0, 0.0); glVertex2f(1.0, 1.0);
+    glTexCoord2d(1.0, 1.0); glVertex2f(1.0, -1.0);
+  glEnd();
+  glDisable(GL_TEXTURE_2D);
+  glutSwapBuffers();
+
+  return;
+}
+
+void reshape(int w, int h)
+{
+  glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+
+  return;
+}
+
+void idle()
+{
+  setTextureData();
+
+  glutPostRedisplay();
+
+  return;
+}
+
+int main(int argc, char* argv[])
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE);
+  glutInitWindowSize(TEX_WIDTH, TEX_HEIGHT);
+  glutInitWindowPosition(100, 100);
+  glutCreateWindow("recommended movie in 2023!!!");
+  init();
+  glutDisplayFunc(display);
+  glutIdleFunc(idle);
+  glutReshapeFunc(reshape);
+  glutMainLoop();
+
+  return 0;
 }
